023e82a06dc0a9cb661cea82b1e4c3b0fb4d4c00
wenwen195 <1034637037@qq.com>
2017-05-01 15:43:30 +0800

    FirstCreat

---
+++ src/dropAllTable.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+/**
+ * Created by ryw on 2017/5/1.
+ */
+public class dropAllTable {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        try {
+            StringBuffer sb = new StringBuffer("");
+            FileReader reader = new FileReader("D:\\17\\GraduationDesign\\gistoolshdp\\allTables.txt");
+            BufferedReader br = new BufferedReader(reader);
+
+            String str = null;
+
+            while ((str = br.readLine()) != null) {
+                sb.append("DROP TABLE " + str + ";\n");
+
+                System.out.println(str);
+            }
+
+            br.close();
+            reader.close();
+
+            // write string to file
+            FileWriter writer = new FileWriter("DropAllTable.sql");
+            BufferedWriter bw = new BufferedWriter(writer);
+            bw.write(sb.toString());
+
+            bw.close();
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/sqlWrite.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+/**
+ * Created by ryw on 2017/5/1.
+ */
+public class sqlWrite {
+
+
+}
+
+
+
+
+
